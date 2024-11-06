ab6fbb85d3ae70ef815da4d5d95a2561b092b6a0
Frankdias92 <frankmcdias@gmail.com>
2024-11-06 00:28:34 -0300

    :parkles: Feat: add prisma and creating a user test

--- api-sippy/src/app.ts
+++ api-sippy/src/app.ts
@@ -1,4 +1,14 @@
+import { PrismaClient } from "@prisma/client";
 import fastify from "fastify";
 
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+    data: {
+        name: 'User Test',
+        email: 'email@email.com'
+    }
+})
